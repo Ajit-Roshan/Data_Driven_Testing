d172bae32a782eefe624c8bd99816e388a7e210f
ajit <ajit4roshan@gmail.com>
2023-06-23 18:43:59 +0530

    extentReports

--- src/test/java/common/Common.java
+++ src/test/java/common/Common.java
@@ -2,9 +2,7 @@ package common;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
--- src/test/java/driver/TestDriver.java
+++ src/test/java/driver/TestDriver.java
@@ -9,6 +9,11 @@ import org.openqa.selenium.WebElement;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 import common.Common;
 
 public class TestDriver extends Common{
@@ -16,6 +21,9 @@ public class TestDriver extends Common{
 	int rowCount ;
 	TestDriver td;
 
+	ExtentReports er;
+	ExtentTest et;
+	ExtentSparkReporter spark;
 
 	@Test(priority = 1)
 	public void test() throws Throwable {
@@ -28,7 +36,12 @@ public class TestDriver extends Common{
 	@Test(priority = 2)
 	public void	getData() throws Throwable {
 
-		
+		er= new ExtentReports();
+		spark= new ExtentSparkReporter("/home/ajit/eclipse_Space/Data_Driven/output/reports.html");
+		er.attachReporter(spark);
+
+
+
 		for(int i= 1 ; i< rowCount; i++) {
 			String id= td.getId(i);
 			String pass= td.getPass(i);
@@ -37,7 +50,7 @@ public class TestDriver extends Common{
 			WebElement uPass=  dr.findElement(By.id ("password"));
 			WebElement signIn=  dr.findElement(By.id ("submit"));
 
-			
+
 			uName.clear();
 			uPass.clear();
 
@@ -45,29 +58,38 @@ public class TestDriver extends Common{
 			uName.sendKeys(id);
 			uPass.sendKeys(pass);
 			signIn.click();
-			
+
 			dr.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
 
 			String currURL= dr.getCurrentUrl();
 			currURL= currURL.toLowerCase();
 
-			//			assertEquals( "successfully" , currURL.contains("successfully") );
 
-			//			Reporter.log(id +" "+pass ,true);
-			//			
+			et= er
+					.createTest("loginTest")
+					.assignAuthor("Ajit")
+					.assignCategory("functional");
+
+
 			if(currURL.contains("successfully")) {
 				td.setData(i, "pass");
+
+				et.log(Status.PASS, "login pass");
+				er.flush();
+
 				dr.findElement(By.xpath("//a[@href='https://practicetestautomation.com/practice-test-login/']")).click();
 
 				dr.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
 			}
 			else {
-				td.setData(i, "fail");		
+				td.setData(i, "fail");	
+
+				et.log(Status.FAIL, "fail cradentials");
+				er.flush();
 			}
-
 		}	
+		
 	}
-
 }
 
 
